d2fb88dcf0b3b86e4661ae33d5632718eb695dbe
Flaton <flaton.contact@gmail.com>
2023-02-07 20:36:57 +0100

    Add talkie walkie items

--- src/main/java/fr/flaton/talkiewalkiemod/TalkieWalkieMod.java
+++ src/main/java/fr/flaton/talkiewalkiemod/TalkieWalkieMod.java
@@ -1,5 +1,6 @@
 package fr.flaton.talkiewalkiemod;
 
+import fr.flaton.talkiewalkiemod.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
@@ -11,10 +12,6 @@ public class TalkieWalkieMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
-		LOGGER.info("Hello Fabric world!");
+		ModItems.registerModItems();
 	}
 }

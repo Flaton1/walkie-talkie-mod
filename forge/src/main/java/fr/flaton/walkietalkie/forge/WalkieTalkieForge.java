36220d307b7c02b11fefd3372e781776537138f1
Flaton <flaton.contact@gmail.com>
2024-02-24 18:04:37 +0100

    Update 1.20.1

--- forge/src/main/java/fr/flaton/walkietalkie/forge/WalkieTalkieForge.java
+++ forge/src/main/java/fr/flaton/walkietalkie/forge/WalkieTalkieForge.java
@@ -6,11 +6,8 @@ import fr.flaton.walkietalkie.WalkieTalkie;
 import fr.flaton.walkietalkie.WalkieTalkieClient;
 import fr.flaton.walkietalkie.client.gui.screen.SpeakerScreen;
 import fr.flaton.walkietalkie.config.ModConfig;
-import fr.flaton.walkietalkie.item.WalkieTalkieItem;
 import fr.flaton.walkietalkie.screen.ModScreenHandlers;
 import net.minecraft.client.gui.screen.ingame.HandledScreens;
-import net.minecraft.client.item.ModelPredicateProviderRegistry;
-import net.minecraft.util.Identifier;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.DistExecutor;
@@ -36,13 +33,6 @@ public class WalkieTalkieForge {
         @SubscribeEvent
         public static void onClientSetup(FMLClientSetupEvent event) {
             HandledScreens.register(ModScreenHandlers.SPEAKER.get(), SpeakerScreen::new);
-
-            ModelPredicateProviderRegistry.registerGeneric(new Identifier(Constants.MOD_ID, "activate"), ((stack, world, entity, seed) -> {
-                if (!stack.hasNbt()) {
-                    return 0;
-                }
-                return stack.getNbt().getBoolean(WalkieTalkieItem.NBT_KEY_ACTIVATE) ? 1.0f : 0.0f;
-            }));
         }
     }
 }
